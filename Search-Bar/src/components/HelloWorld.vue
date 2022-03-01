705332de5cbd7ae87c17c6cdcf247a7e7bd47c5f
Doğukan Korkut <doguuu1998@hotmail.com>
2022-03-01 19:34:38 +0300

    Add search and style for search bar

--- Search-Bar/src/components/HelloWorld.vue
+++ Search-Bar/src/components/HelloWorld.vue
@@ -1,4 +1,18 @@
-<template></template>
+<template>
+  <div class="searchbar-container">
+    <form class="searchbar-container-form" action="https://www.google.com/search" method="get">
+      <input
+        type="text"
+        placeholder="search anything"
+        name="q"
+        class="searchbar-container-form-input"
+      />
+      <button type="submit" class="searchbar-container-form-button">
+        <img src="../assets/images/search.png" />
+      </button>
+    </form>
+  </div>
+</template>
 <script setup>
 </script>
 <style src="./HelloWorld.scss" lang="scss">
